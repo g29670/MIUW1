b1b10e3b73d7295fc8fe6ca6f85b12cf64fb27a0
g29670 <eliasM.2013@yahoo.com>
2013-11-27 22:49:48 -0600

    commited first files to repo

---
+++ js/json.js
@@ -0,0 +1,202 @@
+var json = {
+	"coin1": {
+		"entercoin": ["Coin Style: ", "Penny"],
+		"mdate": ["Mint Date: ", "1930"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-1988"],
+		"worth": ["Is this coin valuable today? ", "Yes"],
+		"coinage": ["What is the value of this coin? ", "3"],
+		"quality": ["The coin quality is: ", "Excellent"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin2": {
+		"entercoin": ["Coin Style: ", "Nickel"],
+		"mdate": ["Mint Date: ", "2003"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-2003"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Excellent"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin3": {
+	    "entercoin": ["Coin Style: ", "Dime"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2005"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin4": {
+	    "entercoin": ["Coin Style: ", "Quarter"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2007"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin5": {
+	    "entercoin": ["Coin Style: ", "Quarter"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-2006"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin6": {
+	    "entercoin": ["Coin Style: ", "Penny"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2005"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin7": {
+	    "entercoin": ["Coin Style: ", "Penny"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2005"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin8": {
+	    "entercoin": ["Coin Style: ", "Penny"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2005"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin9": {
+	    "entercoin": ["Coin Style: ", "Dime"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-1985"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin10": {
+	    "entercoin": ["Coin Style: ", "Quarter"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2000"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin11": {
+	    "entercoin": ["Coin Style: ", "Dime"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-1975"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin12": {
+	    "entercoin": ["Coin Style: ", "Nickel"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-1967"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin13": {
+	    "entercoin": ["Coin Style: ", "Dime"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-1950"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin14": {
+	    "entercoin": ["Coin Style: ", "Quarter"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2010"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin15": {
+	    "entercoin": ["Coin Style: ", "Nickel"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-2003"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin16": {
+	    "entercoin": ["Coin Style: ", "Nickel"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-1987"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin17": {
+	    "entercoin": ["Coin Style: ", "Nickel"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-1999"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin18": {
+	    "entercoin": ["Coin Style: ", "Dime"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-1970"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin19": {
+	    "entercoin": ["Coin Style: ", "Quarter"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Denver"],
+		"edate": ["Date entered into collection:", "01-01-2005"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	},
+	"coin20": {
+	    "entercoin": ["Coin Style: ", "Penny"],
+		"mdate": ["Mint Date: ", "1988"],
+		"mloc": ["Mint Location: ", "Pennsylvania"],
+		"edate": ["Date entered into collection:", "01-01-1937"],
+		"worth": ["Is this coin valuable today? ", "No"],
+		"coinage": ["What is the value of this coin? ", "1"],
+		"quality": ["The coin quality is: ", "Good"],
+		"comments": ["Give information about the coin? ", "Comment on coin"]
+	}
+};
